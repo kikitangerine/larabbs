2baf5a85d944f70fb5f8e990c72575e81092e776
kk <kk@kkdeMacBook-Pro.local>
2020-05-19 22:02:26 +0800

    dingo

--- config/sudosu.php
+++ config/sudosu.php
@@ -3,7 +3,7 @@
 return [
 
     // 允许使用的顶级域名
-    'allowed_tlds' => ['dev', 'local', 'test', 'localhost'],
+    'allowed_tlds' => ['dev', 'local', 'test', 'localhost','1'],
 
     // 用户模型
     'user_model' => App\Models\User::class
